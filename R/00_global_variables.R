c7a0ab03efdcc7af81a0292ed572637caa53552f
Steven Sanderson <spsanderson@gmail.com>
2020-11-24 22:35:32 -0500

    add global var alos

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -2,6 +2,6 @@ globalVariables(
     names = c(
         "text","start_num","num_char","median_value","last_flag","excess","value"
         ,"idx","x","y","los_group","los_index","los_ra_var","n","rar_index",
-        "readmit_rate_bench"
+        "readmit_rate_bench","alos"
     )
 )
