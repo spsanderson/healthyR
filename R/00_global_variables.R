1e450f2d42368027774a2d0cd9df43b45397719d
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-06-08 23:23:23 -0400

    Update 00_global_variables.R

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -2,6 +2,7 @@ globalVariables(
     names = c(
         "text","start_num","num_char","median_value","last_flag","excess","value"
         ,"idx","x","y","los_group","los_index","los_ra_var","n","rar_index",
-        "readmit_rate_bench","alos","record","total_records","prop_of_total"
+        "readmit_rate_bench","alos","record","total_records","prop_of_total",
+        ".cluster"
     )
 )
